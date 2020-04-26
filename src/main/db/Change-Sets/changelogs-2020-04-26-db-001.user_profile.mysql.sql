888ee4648513b06a15b35223a357ce22823b6fbf
Victor N. Skurikhin <vs@lnhost.net>
2020-04-26 20:28:45 +0300

    Добавленна таблица user_profile

---
+++ src/main/db/Change-Sets/changelogs-2020-04-26-db-001.user_profile.mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_profile (
+  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+  login      VARCHAR(256) NOT NULL,
+  hash       VARCHAR(256) NOT NULL,
+  expired    BOOLEAN DEFAULT false NOT NULL,
+  locked     BOOLEAN DEFAULT false NOT NULL
+) ENGINE = InnoDB;
