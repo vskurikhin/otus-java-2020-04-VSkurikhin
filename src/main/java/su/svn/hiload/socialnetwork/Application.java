f1c983d29d948ea776c89231291be9612b79ef15
Victor N. Skurikhin <vs@lnhost.net>
2020-04-26 20:20:00 +0300

    First build.

---
+++ src/main/java/su/svn/hiload/socialnetwork/Application.java
@@ -0,0 +1,13 @@
+package su.svn.hiload.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
